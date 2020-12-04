7a6e30f104ee7b1c1e52d4cb34ac2ee694387a8f
Jeremy Xie <climen712@gmail.com>
2020-12-04 11:23:16 +0800

    rewrite url shortener

--- urlshort/handler.go
+++ urlshort/handler.go
@@ -1,10 +1,7 @@
 package urlshort
 
 import (
-	"fmt"
 	"net/http"
-
-	"github.com/go-yaml/yaml"
 )
 
 // MapHandler will return an http.HandlerFunc (which also
@@ -15,19 +12,13 @@ import (
 // http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		URL := pathsToUrls[r.URL.Path]
-		if URL != "" {
-			http.Redirect(w, r, URL, http.StatusFound)
+		url := r.URL.Path
+		if _, ok := pathsToUrls[url]; ok {
+			http.Redirect(w, r, pathsToUrls[url], http.StatusFound)
 			return
 		}
 		fallback.ServeHTTP(w, r)
 	}
-}
-
-//YAMLMap is built to store path and urls
-type YAMLMap struct {
-	path string `yaml:"path"`
-	URL  string `yaml:"url"`
 }
 
 // YAMLHandler will parse the provided YAML and then return
@@ -47,31 +38,6 @@ type YAMLMap struct {
 // See MapHandler to create a similar http.HandlerFunc via
 // a mapping of paths to urls.
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	pathsUrls, err := parseYAML(yml)
-	if err != nil {
-		return nil, err
-	}
-	mapToUrls := mapBuilder(pathsUrls)
-
-	return MapHandler(mapToUrls, fallback), nil
-}
-
-func parseYAML(ymlInput []byte) (pathsUrls []YAMLMap, err error) {
-	if len(ymlInput) == 0 {
-		fmt.Println("YAML file is empty.")
-		return
-	}
-	err = yaml.Unmarshal(ymlInput, &pathsUrls)
-	return
-}
-
-func mapBuilder(ymlMap []YAMLMap) map[string]string {
-	if len(ymlMap) == 0 || ymlMap == nil {
-		return nil
-	}
-	mapToUrls := make(map[string]string)
-	for _, v := range ymlMap {
-		mapToUrls[v.path] = v.URL
-	}
-	return mapToUrls
+	// TODO: Implement this...
+	return nil, nil
 }
--- urlshort/main/main.go
+++ urlshort/main/main.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"gameswithgo/urlshort"
 	"net/http"
-	// "github.com/gophercises/urlshort"
-)
 
-var (
-	ymlpath = flag.String("ymlpath", "../data/test.yml", "Path to the yaml file")
+	"github.com/gophercises/urlshort"
 )
 
 func main() {
@@ -25,11 +20,11 @@ func main() {
 	// Build the YAMLHandler using the mapHandler as the
 	// fallback
 	yaml := `
-    - path: /urlshort
-	   url: https://github.com/gophercises/urlshort
-    - path: /urlshort-final
-       url: https://github.com/gophercises/urlshort/tree/solution
-	`
+- path: /urlshort
+  url: https://github.com/gophercises/urlshort
+- path: /urlshort-final
+  url: https://github.com/gophercises/urlshort/tree/solution
+`
 	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
 	if err != nil {
 		panic(err)
